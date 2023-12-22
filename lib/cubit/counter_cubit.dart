b4bdf72c9f3a7ead86c5a238733844e5ab81d12d
Yana Mulyana <yanamyn15@gmail.com>
2023-12-22 22:36:50 +0700

    first commit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print("SimpleBlocObserver");
+    print('${bloc.runtimeType} $change');
+  }
+}
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit(int initialState): super(initialState);
+
+  void increment() => emit(state + 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print("change");
+    print(change);
+  }
+}
+
+class DeCounterCubit extends Cubit<int> {
+  DeCounterCubit(int initialState): super(initialState);
+
+  void decrement() => emit(state - 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print("change");
+    print(change);
+  }
+}
+
